bbaf4255d51b0b5293119bb4b7e6a404b1c74915
Timothy Lock <lock.k.timothy@gmail.com>
2019-01-20 04:41:40 -0500

    Shhhhh (new timings)

--- onboard/main.py
+++ onboard/main.py
@@ -9,7 +9,8 @@ import math
 def uploadImages(pre, lat, long):
     try:
         for x in range(1, 4):
-            for y in range(1, 4):
+            for y in range(1, 4 ):
+                time.sleep(0.5)
                 with open('./data/' + pre + '_0' + str(x) + '_0' + str(y) + '.png', 'rb') as f:
                     multipart_form_data = {
                         'img.jpg': ('img.jpg', f),
@@ -23,7 +24,7 @@ def uploadImages(pre, lat, long):
 
 # Get reference time
 start = timeit.default_timer()
-timeToReset = 60
+timeToReset = 75
 
 startLat = 42.848620
 endLat = 42.848683
@@ -36,6 +37,7 @@ while True:
     curTime = timeit.default_timer()
     currLat = startLat + ((curTime - start)/(timeToReset * 1000)) * (endLat - startLat)
     currLng = startLng + ((curTime - start)/(timeToReset * 1000)) * (endLng - startLng)
+    
     if curTime >= start + 12 and curTime <= start + 14:
         print(str(curTime) + " - sending image for processing FR1")
         image_slicer.slice('./data/12.jpg', 9)
@@ -44,11 +46,11 @@ while True:
         print(str(curTime) + " - sending image for processing FR2")
         image_slicer.slice('./data/27.jpg', 9)
         uploadImages("27", currLat , currLng)
-    if curTime >= start + 45 and curTime <= start + 47:
+    if curTime >= start + 47 and curTime <= start + 49:
         print(str(curTime) + " - sending image for processing FR3")
         image_slicer.slice('./data/45.jpg', 9)
         uploadImages("45", currLat , currLng)
-    if curTime >= start + 49 and curTime <= start + 51:
+    if curTime >= start + 59 and curTime <= start + 61:
         print(str(curTime) + " - sending image for processing FR4")
         image_slicer.slice('./data/49.jpg', 9)
         uploadImages("49", currLat , currLng)
